f5825c2f7cd805435fe0491de7d7946cb61733be
khg <ryouhei123123@gmail.com>
2020-03-14 14:46:31 +0900

    DataGrid頑張ってるなう  いったんStyleについてはすべてコピペ

--- KhgDialogModule1/ViewModels/ViewAViewModel.cs
+++ KhgDialogModule1/ViewModels/ViewAViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using khgWPFLearn_CustomUI.BindingBase;
+using khgWPFLearn_CustomUI.Dialogs;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
+using System.ComponentModel;
+
 
 namespace KhgDialogModule1.ViewModels
 {
     public class ViewAViewModel : KHGDialogBindingBase
     {
-        
+
 
         private string _message;
         public string Message
@@ -21,6 +24,8 @@ namespace KhgDialogModule1.ViewModels
         public ReactiveCollection<string> ListTest { get; set; } = new ReactiveCollection<string>();
         public ReactiveProperty<string> SelectedTest { get; set; } = new ReactiveProperty<string>();
 
+        public ReactiveCollection<DataGridTest> GridTest { get; set; } = new ReactiveCollection<DataGridTest>();
+        public ReactiveProperty<DataGridTest> GridSelectTest { get; set; } = new ReactiveProperty<DataGridTest>();
         public ViewAViewModel()
         {
             Message = "View A from your Prism Module";
@@ -30,7 +35,53 @@ namespace KhgDialogModule1.ViewModels
             ListTest.Add("２こめ");
             ListTest.Add("３こめ");
 
+            GridTest.Add(new DataGridTest() { No = 1, Column1 = "いち-1", Column2 = "いち-2" });
+            GridTest.Add(new DataGridTest() { No = 2, Column1 = "に-1", Column2 = "に-2" });
+            GridTest.Add(new DataGridTest() { No = 3, Column1 = "さん-1", Column2 = "さん-2" });
+            GridTest.Add(new DataGridTest() { No = 4, Column1 = "よｎ-1", Column2 = "よん-2" });
+
             OkbuttonClick.Subscribe(_ => { CloseDialog(new Prism.Services.Dialogs.DialogResult(Prism.Services.Dialogs.ButtonResult.OK)); }).AddTo(Disposer);
+        }
+    }
+
+
+    public class DataGridTest : KHGBindingBase
+    {
+
+        private int no;
+
+
+        private string column1;
+
+
+        private string column2;
+
+        [DisplayName("番号")]
+        public int No
+        {
+            get => no;
+            set
+            {
+                SetProperty(ref no, value);
+            }
+        }
+
+        [DisplayName("その1")]
+        public string Column1
+        {
+            get => column1; set
+            {
+                SetProperty(ref column1, value);
+            }
+        }
+
+        [DisplayName("その2")]
+        public string Column2
+        {
+            get => column2; set
+            {
+                SetProperty(ref column2, value);
+            }
         }
     }
 }
---
+++ khgWPFLearn_CustomUI/Behavior/DataGridExtention.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace khgWPFLearn_CustomUI.Behavior
+{
+    public class DataGridExtention : Behavior<DataGrid>
+    {
+
+        public DataGridExtention()
+        {
+        }
+
+        // 要素にアタッチされたときの処理。大体イベントハンドラの登録処理をここでやる
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.AutoGeneratingColumn += dataGrid_AutoGeneratingColumn;
+
+        }
+
+        // 要素にデタッチされたときの処理。大体イベントハンドラの登録解除をここでやる
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.AutoGeneratingColumn -= dataGrid_AutoGeneratingColumn;
+
+        }
+
+        private void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs eventArgs)
+        {
+            if (eventArgs.PropertyDescriptor is PropertyDescriptor descriptor)
+            {
+                eventArgs.Column.Header = descriptor.DisplayName ?? descriptor.Name;
+            }
+            else
+            {
+                eventArgs.Cancel = true;
+            }
+        }
+    }
+}
